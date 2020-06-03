b4c7f21bfc25529017abdcfbfa0a01f23bd9b572
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-06-03 14:05:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,8 +1,9 @@
-
+ 
 
 def get_user_input
-  gets.chomp
+   gets.chomp
 end
+
 
 def prompt_user
   puts "What would you like to do?"
@@ -11,9 +12,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
@@ -21,4 +22,6 @@ end
 def runner
   prompt_user
   selection(get_user_input)
+  
 end
+
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -7,16 +8,9 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
-end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
-end
+def engage 
 
-def engage
-  date = generate_star_date
-  state_log(date)
-  greet_crew(crew)
-end
+  puts state_log(star_date)
+  puts crew_greeting
+ end 
